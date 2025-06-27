75a461d25504efa147b9784ff588f54b2b49b09d
Nouval Rizky <nouvalr@gmail.com>
2025-06-28 01:05:34 +0800

    fix: dense layout on register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -24,7 +24,7 @@ const RegsiterPage = () => {
         <div className="absolute inset-0 bg-gradient-to-b lg:bg-gradient-to-r from-transparent via-white/95 to-white" />
       </div>
       <div className="flex container mx-auto justify-end items-end lg:items-center h-full relative z-10">
-        <div className="w-full lg:max-w-lg flex flex-col gap-8">
+        <div className="w-full lg:max-w-lg flex flex-col gap-8 py-8">
           <TravelixLogoHorizontal className="mx-auto" width={160} />
           <RegisterForm />
         </div>
--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -104,68 +104,73 @@ const RegisterForm = () => {
             labelPlacement="outside"
             placeholder="Enter your name"
           />
-          <Input
-            {...register("email")}
-            errorMessage={errors.email?.message}
-            isInvalid={!!errors.email?.message}
-            label="Email"
-            labelPlacement="outside"
-            placeholder="Enter your email"
-          />
-          <Input
-            {...register("password")}
-            endContent={
-              <button
-                className="text-gray-700"
-                type="button"
-                onClick={togglePasswordVisible}
-              >
-                {isPasswordVisible ? (
-                  <EyeOff className="size-5" />
-                ) : (
-                  <Eye className="size-5" />
-                )}
-              </button>
-            }
-            errorMessage={errors.password?.message}
-            isInvalid={!!errors.password?.message}
-            label="Password"
-            labelPlacement="outside"
-            placeholder="Enter your password"
-            type={isPasswordVisible ? "text" : "password"}
-          />
-          <Input
-            {...register("passwordRepeat")}
-            endContent={
-              <button
-                className="text-gray-700"
-                type="button"
-                onClick={togglePasswordVisible}
-              >
-                {isPasswordVisible ? (
-                  <EyeOff className="size-5" />
-                ) : (
-                  <Eye className="size-5" />
-                )}
-              </button>
-            }
-            errorMessage={errors.passwordRepeat?.message}
-            isInvalid={!!errors.passwordRepeat?.message}
-            label="Password Repeat"
-            labelPlacement="outside"
-            placeholder="Enter your password repeat"
-            type={isPasswordVisible ? "text" : "password"}
-          />
-          <Input
-            {...register("phoneNumber")}
-            errorMessage={errors.phoneNumber?.message}
-            isInvalid={!!errors.phoneNumber?.message}
-            label="Phone Number"
-            labelPlacement="outside"
-            min={0}
-            placeholder="Enter your phone number"
-            type="number"
-          />
+          <div className="flex gap-3 items-start">
+            <Input
+              {...register("email")}
+              errorMessage={errors.email?.message}
+              isInvalid={!!errors.email?.message}
+              label="Email"
+              labelPlacement="outside"
+              placeholder="Enter your email"
+            />
+            <Input
+              {...register("phoneNumber")}
+              errorMessage={errors.phoneNumber?.message}
+              isInvalid={!!errors.phoneNumber?.message}
+              label="Phone Number"
+              labelPlacement="outside"
+              min={0}
+              placeholder="Enter your phone number"
+              type="number"
+            />
+          </div>
+          <div className="flex gap-3 items-start">
+            <Input
+              {...register("password")}
+              endContent={
+                <button
+                  className="text-gray-700"
+                  type="button"
+                  onClick={togglePasswordVisible}
+                >
+                  {isPasswordVisible ? (
+                    <EyeOff className="size-5" />
+                  ) : (
+                    <Eye className="size-5" />
+                  )}
+                </button>
+              }
+              errorMessage={errors.password?.message}
+              isInvalid={!!errors.password?.message}
+              label="Password"
+              labelPlacement="outside"
+              placeholder="Enter your password"
+              type={isPasswordVisible ? "text" : "password"}
+            />
+            <Input
+              {...register("passwordRepeat")}
+              endContent={
+                <button
+                  className="text-gray-700"
+                  type="button"
+                  onClick={togglePasswordVisible}
+                >
+                  {isPasswordVisible ? (
+                    <EyeOff className="size-5" />
+                  ) : (
+                    <Eye className="size-5" />
+                  )}
+                </button>
+              }
+              errorMessage={errors.passwordRepeat?.message}
+              isInvalid={!!errors.passwordRepeat?.message}
+              label="Password Repeat"
+              labelPlacement="outside"
+              placeholder="Enter your password repeat"
+              type={isPasswordVisible ? "text" : "password"}
+            />
+          </div>
+
           <ImageInputField
             image={getValues("profilePictureFile")}
             label="Profile Picture (Optional)"
