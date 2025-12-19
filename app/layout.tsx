e41b94a392096179b051c3ccc0074c887799023a
Nouval Rizky <nouvalr@gmail.com>
2025-12-19 17:23:39 +0000

    deps: enable clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Providers } from "./providers";
 
 import { fontGeist, fontManrope } from "@/config/fonts";
 import ScrollToTop from "@/components/ScrollToTop";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -34,7 +35,20 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
+      <head>
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+      >
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "uo27f5ddc4");
+        `}
+      </Script>
+      </head>
       <body
         className={clsx(
           "min-h-screen text-foreground bg-background font-manrope antialiased",
